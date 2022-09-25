268e4eefa030ff67c0651cb01c12ea0c9ccfa656
rezlevi <113434088+rezlevi@users.noreply.github.com>
2022-09-25 18:05:09 +0200

    Factorial with input

--- Factorial.py
+++ Factorial.py
@@ -4,6 +4,15 @@ def factorial(x):
     else:
         return (x * factorial(x - 1))
 
-number = int(input("Enter the number: "))
+x = int(input("Enter the number: "))
 
-print("The factorial of", number, "is",factorial(number))
+print("The factorial of", x, "with recursion is",factorial(5))
+
+def factorialLoop(x):
+    num = 1
+    while x >=1:
+        num = num * x
+        x = x - 1
+    return num
+
+print("The factorial of", x, "with iteration is",factorialLoop(5))
