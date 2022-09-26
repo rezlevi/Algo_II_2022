5f4102c5299d15bf297b3c58dbb7e1595b857ba7
rezlevi <113434088+rezlevi@users.noreply.github.com>
2022-09-26 22:05:46 +0200

    Recursive and Iterative.py

--- Fibonacci numbers.py
+++ Fibonacci numbers.py
@@ -1,16 +1,37 @@
 ## with recursion
 
-def fib(x):
+def fibRec(x):
     if (x == 0):
         return 0
     if (x == 1 or x == 0):
         return 1
     else:
-        return (fib(x - 1) + fib(x - 2))
+        return (fibRec(x - 1) + fibRec(x - 2))
+
+
+
+
+## with iteration
+
+def fibIte(x):
+    a = 0
+    b = 1
+
+    print(a, b, end=" ")
+    while (x - 2):
+        c = a + b
+        a, b = b, c
+        print(c, end=" ")
+        x = x - 1
+
+
 
 x = int(input("Give me the length of the fibonacci numbers: "))
 
-for i in range(0,x):
-    print(fib(i),end=" ")
+print(fibIte(x))
 
-## with iteration
+for i in range(0,x):
+    print(fibRec(i),end=" ")
+
+
+
