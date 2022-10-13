c5cf80315f028e46b4f53165e03890ceb28aa65d
Levente Janos Rez <ice3886@gmail.com>
2022-10-13 12:31:22 +0200

    more done

--- Temazaro_feladat.py
+++ Temazaro_feladat.py
@@ -6,18 +6,22 @@ def factorial(x):
     else:
         return (x * factorial(x - 1))
 
+
 x = int(input("Enter the number: "))
 
-print("The factorial of", x, "with recursion is",factorial(5))
+print("The factorial of", x, "with recursion is", factorial(5))
+
 
 def factorialLoop(x):
     num = 1
-    while x >=1:
+    while x >= 1:
         num = num * x
         x = x - 1
     return num
 
-print("The factorial of", x, "with iteration is",factorialLoop(5))
+
+print("The factorial of", x, "with iteration is", factorialLoop(5))
+
 
 ##Második feladat: Hanoi tornyai
 
@@ -29,7 +33,8 @@ def fibRec(x):
     if x == 1 or x == 0:
         return 1
     else:
-        return fibRec(x - 1) + fibRec(x - 2) + fibRec(x-3)
+        return fibRec(x - 1) + fibRec(x - 2) + fibRec(x - 3)
+
 
 ##Ötödik feladat: Előző kettő összege
 
@@ -65,6 +70,7 @@ print(fibIte(x))
 for i in range(0, x):
     print(fibRec(i), end=" ")
 
+
 ##Hatodik feladat: Tiz hatványai
 
 def power(P):
@@ -78,6 +84,7 @@ if __name__ == '__main__':
     P = 2
 
     print(power(P))
+
 
 ##Hetedik feladat: Kettő hatványai algoritmus
 
@@ -93,3 +100,80 @@ if __name__ == '__main__':
 
     print(power(P))
 
+
+##Két inputos feladatok
+
+##Első feladat: LNKO nyers
+
+def calc_gcd(num1, num2):
+    if (num2 == 0):
+        return num1
+    else:
+        return calc_gcd(num2, num1 % num2)
+
+
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+result = calc_gcd(num1, num2)
+print("GCD is : {}".format(result))
+
+##Második feladat: LNKO iterativ
+
+a = int(input("What's the first number? "))
+b = int(input("What's the second number? "))
+r = a % b
+while r:
+    a = b
+    b = r
+    r = a % b
+print('GCD is:', b)
+
+
+##Negyedik feladat: LNKO rekurziv
+
+def gcd(x, y):
+    if y == 0:
+        return x
+    else:
+        return gcd(y, x % y)
+
+
+num_one = int(input('Enter a value for x: '))
+num_two = int(input('Enter a value for y: '))
+if num_two == 0:
+    print(num_one)
+else:
+    print(gcd(num_one, num_two))
+
+
+##Ötödik feladat: LKKT
+
+def compute_lcm(x, y):
+    if x > y:
+        greater = x
+    else:
+        greater = y
+
+    while (True):
+        if ((greater % x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
+
+    return lcm
+
+
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+print("The LCM is", compute_lcm(num1, num2))
+
+
+def lcm(x, y):
+    t = x % y
+    if t == 0: return x
+    return x * lcm(y, t) / t
+
+
+print("The LCM is: ", lcm(num1, num2))
